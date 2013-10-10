9acf4fdf99e0a251035e137ac542ab1e10f39f47
liufeiit <liufei_it@126.com>
2013-10-10 23:44:18 +0800

    SpringExt

--- src/main/java/org/springframework/ext/package-info.java
+++ src/main/java/org/springframework/ext/package-info.java
@@ -2,6 +2,6 @@
  * 
  * @author 刘飞 E-mail:liufei_it@126.com
  * @version 1.0
- * @since Oct 10, 2013 4:24:10 PM
+ * @since 2013年10月10日 下午11:37:10
  */
 package org.springframework.ext;
---
+++ src/test/java/org/springframework/ext/hello/MessagePrinter.java
@@ -0,0 +1,21 @@
+package org.springframework.ext.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 
+ * @author 刘飞 E-mail:liufei_it@126.com
+ * @version 1.0
+ * @since 2013年10月10日 下午11:39:19
+ */
+@Component
+public class MessagePrinter {
+
+    @Autowired
+    private MessageService service;
+
+    public void printMessage() {
+        System.out.println(this.service.getMessage());
+    }
+}
