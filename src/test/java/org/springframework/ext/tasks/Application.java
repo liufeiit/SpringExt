b7a9f7fb7db1a753fbcfa255d666a8ef0e8bee80
liufei <liufei_it@126.com>
2013-10-11 17:01:35 +0800

    SpringExt

---
+++ src/test/java/org/springframework/ext/tasks/Application.java
@@ -0,0 +1,10 @@
+package org.springframework.ext.tasks;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ScheduledTasks.class);
+    }
+}
---
+++ src/test/java/org/springframework/ext/validating/Application.java
@@ -0,0 +1,43 @@
+package org.springframework.ext.validating;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring3.SpringTemplateEngine;
+import org.thymeleaf.spring3.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+    
+    @Bean
+    ServletContextTemplateResolver templateResolver() {
+        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        return resolver;
+    }
+    
+    @Bean
+    SpringTemplateEngine engine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolver());
+        return engine;
+    }
+    
+    @Bean
+    ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(engine());
+        return viewResolver;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+    
+}
