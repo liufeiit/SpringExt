d5dac14ac47ea8f99f4c35908b42aa41ffdfe0e1
liufeiit <liufei_it@126.com>
2013-10-11 23:03:21 +0800

    d

--- build/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd ..
-mvn clean install -Pprd -Dmaven.test.skip=true
-cp /home/zhy/ruoogle/workspace/scheduler/target/scheduler.war /home/zhy/jboss-as-7.1.1.Final/standalone/deployments/
-
--- src/main/java/org/springframework/ext/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * 
- * @author 刘飞 E-mail:liufei_it@126.com
- * @version 1.0
- * @since 2013年10月10日 下午11:37:10
- */
-package org.springframework.ext;
