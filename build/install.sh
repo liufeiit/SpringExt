2df054295c9abea2c9a0927ddff9714bfa461e9a
liufeiit <liufei_it@126.com>
2013-10-28 22:48:46 +0800

    s

--- build/install.sh
+++ build/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ..
 mvn clean install -Pprd -Dmaven.test.skip=true
-cp /home/zhy/ruoogle/workspace/scheduler/target/scheduler.war /home/zhy/jboss-as-7.1.1.Final/standalone/deployments/
+cp /home/lf/workspace/scheduler/target/scheduler.war /home/lf/dev/jboss-as-7.1.1.Final/standalone/deployments/
 
